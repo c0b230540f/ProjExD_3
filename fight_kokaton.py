c6fc3418010e9656c9a770dad16203db72dc65a5
kanato <c0b230540f@edu.teu.ac.jp>
2024-06-25 15:48:06 +0900

    練習5-1

--- fight_kokaton.py
+++ fight_kokaton.py
@@ -149,7 +149,7 @@ def main():
     bird = Bird((300, 200))
     beam = None
     #bomb = Bomb((255, 0, 0), 10)
-    bombs = Bomb[((255, 0, 0), 10) for i in range(NUM_OF_BOMBS)]
+    bombs = [Bomb((255, 0, 0), 10) for i in range(NUM_OF_BOMBS)]
     clock = pg.time.Clock()
     tmr = 0
     while True:
@@ -161,29 +161,27 @@ def main():
                 beam = Beam(bird)            
         screen.blit(bg_img, [0, 0])
         
-        if bomb is not None:
+        for bomb in bombs:
             if bird.rct.colliderect(bomb.rct):
                 # ゲームオーバー時に，こうかとん画像を切り替え，1秒間表示させる
                 bird.change_img(8, screen)
                 pg.display.update()
                 time.sleep(1)
-                fonto = pg.font.Font(None,80)
-                txt = fonto.render("GAME OVER", True, (255, 0, 0))
-                screen.blit(txt, [WIDTH/2-150, HEIGHT/2])
-                pg.display.update()
-                time.sleep(5)
                 return
-        if beam is not None and bomb is not None:
-            if bomb.rct.colliderect(beam.rct):
-                bomb = None
-                beam = None
-                bird.change_img(6, screen)
+
+        for i in range(len(bombs)):   
+            if beam is not None:
+                if bombs[i].rct.colliderect(beam.rct):
+                    bombs[i] = None
+                    beam = None
+                    bird.change_img(6, screen)
+        bombs = [bomb for bomb in bombs if bomb is not None]
 
         key_lst = pg.key.get_pressed()
         bird.update(key_lst, screen)
         if beam is not None:
             beam.update(screen)   
-        if bomb is not None:
+        for bomb in bombs:
             bomb.update(screen)
         pg.display.update()
         tmr += 1
